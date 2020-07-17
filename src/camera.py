b6316893e95f4dd075c44657a6c36a9f644c3f56
GusPetito <guspetito@gmail.com>
2020-07-16 19:17:35 -0500

    Basic options

--- src/camera.py
+++ src/camera.py
@@ -4,26 +4,41 @@ import cv2
 class Camera:
     # Use this function to edit the frame in the camera loop. Must return an np array of the same shape as frame
     # if you override
-    def _edit_frame(self, frame):
+    def _edit_frame(self, frame, frame_counter):
         return frame
 
-    def run_camera(self):
+    # Runs the camera loop.
+    # frame_delay is the minimum amount of milliseconds between each frame.
+    # closing_keys are the keys to close the program as characters. Empty list for no keys, None for all keys
+    def run_camera(self, frame_delay=1, closing_keys=['q']):
         cap = cv2.VideoCapture(0)
 
+        ret, frame = cap.read()
+        cv2.imshow('frame', frame)
+
         # The loop to run the program
-        is_running_correctly = cap.isOpened()
-        while is_running_correctly:
+        frame_counter = 0
+        while True:
             ret, frame = cap.read()
             if not ret:
                 break
 
             # Edit the frame
-            frame = self._edit_frame(frame)
+            frame = self._edit_frame(frame, frame_counter)
 
-            # Show the frame, and stop if the user clicks 'q'
+            # Show the frame'
             cv2.imshow('frame', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+
+            # Stop if the user clicks a closing character or closes the window
+            key = cv2.waitKey(frame_delay)
+            if closing_keys is None and key != -1:
                 break
+            if closing_keys is not None and key in map(lambda stop_key: ord(stop_key), closing_keys):
+                break
+            if cv2.getWindowProperty('frame', cv2.WND_PROP_VISIBLE) < 1:
+                break
+
+            frame_counter += 1
 
         # Release the capture and destroy the window after the user quits
         cap.release()
